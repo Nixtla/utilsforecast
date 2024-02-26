correct calculation of quantile loss (#63)

utilsforecast/losses.py
@@ -392,18 +392,24 @@ def quantile_loss(
         dataframe with one row per id and one column per model.
     """
     if isinstance(df, pd.DataFrame):
-        delta_y = df[models].sub(df[target_col], axis=0).abs()
-        res = (
-            np.maximum(q * delta_y, (q - 1) * delta_y)
-            .groupby(df[id_col], observed=True)
-            .mean()
-        )
-        res.index.name = id_col
-        res = res.reset_index()
+        res: Optional[pd.DataFrame] = None
+        for model in models:
+            delta_y = df[target_col].sub(df[model], axis=0)
+            model_res = (
+                np.maximum(q * delta_y, (q - 1) * delta_y)
+                .groupby(df[id_col], observed=True)
+                .mean()
+                .rename(model)
+                .reset_index()
+            )
+            if res is None:
+                res = model_res
+            else:
+                res[model] = model_res[model]
     else:
 
         def gen_expr(model):
-            delta_y = pl.col(model).sub(pl.col(target_col)).abs()
+            delta_y = pl.col(target_col).sub(pl.col(model))
             try:
                 col_max = pl.max_horizontal([q * delta_y, (q - 1) * delta_y])
             except AttributeError:
@@ -413,7 +419,7 @@ def quantile_loss(
         res = _pl_agg_expr(df, models, id_col, gen_expr)
     return res
 
-# %% ../nbs/losses.ipynb 57
+# %% ../nbs/losses.ipynb 58
 def mqloss(
     df: DataFrame,
     models: List[str],
@@ -479,7 +485,7 @@ def mqloss(
                 res = res.join(result, on=id_col)
     return res
 
-# %% ../nbs/losses.ipynb 61
+# %% ../nbs/losses.ipynb 63
 def coverage(
     df: DataFrame,
     models: List[str],
@@ -536,7 +542,7 @@ def coverage(
         res = _pl_agg_expr(df, models, id_col, gen_expr)
     return res
 
-# %% ../nbs/losses.ipynb 65
+# %% ../nbs/losses.ipynb 67
 def calibration(
     df: DataFrame,
     models: List[str],
@@ -586,7 +592,7 @@ def calibration(
         res = _pl_agg_expr(df, models, id_col, gen_expr)
     return res
 
-# %% ../nbs/losses.ipynb 69
+# %% ../nbs/losses.ipynb 71
 def scaled_crps(
     df: DataFrame,
     models: List[str],