a54fafbd947c2147f007fc5d614bb0bba789066f
deven367 <masterdeven@gmail.com>
2025-06-06 22:01:36 -0400

    use py file instead of console script

--- bash_scripts/extract_test.sh
+++ bash_scripts/extract_test.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-mkdir -p ../tests3
-mkdir -p ../tests2
+echo Python: $(which python)
+source /Users/deven367/miniforge3/bin/activate
+conda activate nixtla
+
+echo Activated env: $(which python)
+
+
+mkdir -p ../tests
 
 tst_flags='datasets distributed matplotlib polars pyarrow scipy'
 
 nbs=$(ls ../nbs/*.ipynb)
 # echo "Available notebooks: $nbs"
 
-for flag in $tst_flags; do
-    # echo "Extracting $flag"
 
-    for nb in $nbs; do
-        # get name of notebook without extension
-        nb_name=$(basename "$nb" .ipynb)
+# this approach3 that was discussed on Slack
+# mkdir -p ../tests3
+# for flag in $tst_flags; do
+#     # echo "Extracting $flag"
 
-        # echo "Processing notebook: $nb"
-        # print_dir_in_nb "$nb" --dir_name no_dir_and_dir --dir "$flag" >> "../tests/test_$flag_$nb_name.py"
-        print_dir_in_nb "$nb" --dir_name no_dir_and_dir --dir "$flag" >> "../tests3/test_${flag}_$nb_name.py"
-    done
-done
+#     for nb in $nbs; do
+#         # get name of notebook without extension
+#         nb_name=$(basename "$nb" .ipynb)
+
+#         # echo "Processing notebook: $nb"
+#         # print_dir_in_nb "$nb" --dir_name no_dir_and_dir --dir "$flag" >> "../tests/test_$flag_$nb_name.py"
+#         print_dir_in_nb "$nb" --dir_name no_dir_and_dir --dir "$flag" >> "../tests3/test_${flag}_$nb_name.py"
+#     done
+# done
 
 for nb in $nbs; do
     # get name of notebook without extension
@@ -27,5 +36,5 @@ for nb in $nbs; do
 
     # echo "Processing notebook: $nb"
     # print_dir_in_nb "$nb" --dir_name no_dir_and_dir --dir "$flag" >> "../tests/test_$flag_$nb_name.py"
-    print_dir_in_nb "$nb" --dir_name get_all_tests >> "../tests2/test_$nb_name.py"
+    python cli.py "$nb" --dir_name get_all_tests >> "../tests/test_$nb_name.py"
 done
