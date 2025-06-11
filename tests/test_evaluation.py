597f7d95c45d95526ad8dcf106a6c82f68e84133
deven367 <masterdeven@gmail.com>
2025-06-11 14:56:31 -0400

    fix tests with fixtures

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -10,6 +10,7 @@ import polars as pl
 import polars.testing
 from datasetsforecast.evaluation import accuracy as ds_evaluate
 from pyspark.sql import SparkSession
+import pytest
 
 import utilsforecast.processing as ufp
 from utilsforecast.data import generate_series
@@ -28,36 +29,52 @@ from utilsforecast.losses import (
     smape,
 )
 
-series = generate_series(10, n_models=2, level=[80, 95])
-series["unique_id"] = series["unique_id"].astype("int")
-models = ["model0", "model1"]
-metrics = [
-    mae,
-    mse,
-    rmse,
-    mape,
-    smape,
-    partial(mase, seasonality=7),
-    quantile_loss,
-    mqloss,
-    coverage,
-    calibration,
-    scaled_crps,
-]
-def test_evaluate():
+
+@pytest.fixture
+def setup_series():
+    series = generate_series(10, n_models=2, level=[80, 95])
+    series["unique_id"] = series["unique_id"].astype("int")
+    return series
+
+
+@pytest.fixture
+def setup_models():
+    return ["model0", "model1"]
+
+
+@pytest.fixture
+def setup_metrics():
+    return [
+        mae,
+        mse,
+        rmse,
+        mape,
+        smape,
+        partial(mase, seasonality=7),
+        quantile_loss,
+        mqloss,
+        coverage,
+        calibration,
+        scaled_crps,
+    ]
+
+
+def test_evaluate(setup_series, setup_models, setup_metrics):
     evaluation = evaluate(
-        series,
-        metrics=metrics,
-        models=models,
-        train_df=series,
+        setup_series,
+        metrics=setup_metrics,
+        models=setup_models,
+        train_df=setup_series,
         level=[80, 95],
     )
-    summary = evaluation.drop(columns="unique_id").groupby("metric").mean().reset_index()
+    summary = (
+        evaluation.drop(columns="unique_id").groupby("metric").mean().reset_index()
+    )
 
     series_pl = generate_series(10, n_models=2, level=[80, 95], engine="polars")
     pl_evaluation = evaluate(
         series_pl,
-        metrics=metrics,
+        metrics=setup_metrics,
         train_df=series_pl,
         level=[80, 95],
     ).drop("unique_id")
@@ -68,7 +85,11 @@ def test_evaluate():
     )
     pl.testing.assert_frame_equal(
         evaluate(
-            series_pl, metrics=metrics, train_df=series_pl, level=[80, 95], agg_fn="mean"
+            series_pl,
+            metrics=setup_metrics,
+            train_df=series_pl,
+            level=[80, 95],
+            agg_fn="mean",
         ).sort("metric"),
         pl_summary.sort("metric"),
     )
@@ -77,35 +98,24 @@ def test_evaluate():
 def daily_mase(y, y_hat, y_train):
     return ds_losses.mase(y, y_hat, y_train, seasonality=7)
 
-def test_datasets_evaluate():
+
+def test_datasets_evaluate(setup_series, setup_models, setup_metrics):
     level = [80, 95]
     for agg_fn in [None, "mean"]:
         uf_res = evaluate(
-            series,
-            metrics=metrics,
-            models=models,
-            train_df=series,
+            setup_series,
+            metrics=setup_metrics,
+            models=setup_models,
+            train_df=setup_series,
             level=level,
             agg_fn=agg_fn,
         )
         agg_by = None if agg_fn == "mean" else ["unique_id"]
         ds_res = ds_evaluate(
-            series,
-            metrics=[
-                ds_losses.mae,
-                ds_losses.mse,
-                ds_losses.rmse,
-                ds_losses.mape,
-                daily_mase,
-                ds_losses.smape,
-                ds_losses.quantile_loss,
-                ds_losses.mqloss,
-                ds_losses.coverage,
-                ds_losses.calibration,
-                ds_losses.scaled_crps,
-            ],
+            setup_series,
+            metrics=setup_metrics,
             level=level,
-            Y_df=series,
+            Y_df=setup_series,
             agg_by=agg_by,
         )
         ds_res["metric"] = ds_res["metric"].str.replace("-", "_")
@@ -113,7 +123,9 @@ def test_datasets_evaluate():
         ds_res["metric"] = ds_res["metric"].str.replace("lv_", "level")
         ds_res["metric"] = ds_res["metric"].str.replace("daily_mase", "mase")
         # utils doesn't multiply pct metrics by 100
-        ds_res.loc[ds_res["metric"].str.startswith("coverage"), ["model0", "model1"]] /= 100
+        ds_res.loc[
+            ds_res["metric"].str.startswith("coverage"), ["model0", "model1"]
+        ] /= 100
         ds_res.loc[ds_res["metric"].eq("mape"), ["model0", "model1"]] /= 100
         # we report smape between 0 and 1 instead of 0-200
         ds_res.loc[ds_res["metric"].eq("smape"), ["model0", "model1"]] /= 200
@@ -132,28 +144,31 @@ def test_datasets_evaluate():
         )
 
 
-def test_distributed_evaluate():
-    level=[80, 95]
+def test_distributed_evaluate(setup_series):
+    level = [80, 95]
     if sys.version_info >= (3, 9):
         spark = SparkSession.builder.getOrCreate()
         spark.sparkContext.setLogLevel("FATAL")
-        dask_df = dd.from_pandas(series, npartitions=2)
-        spark_df = spark.createDataFrame(series).repartition(2)
+        dask_df = dd.from_pandas(setup_series, npartitions=2)
+        spark_df = spark.createDataFrame(setup_series).repartition(2)
         for distributed_df, use_train in product([dask_df, spark_df], [True, False]):
             distr_metrics = [rmse, mae]
             if use_train:
                 distr_metrics.append(partial(mase, seasonality=7))
-                local_train = series
+                local_train = setup_series
                 distr_train = distributed_df
             else:
                 local_train = None
                 distr_train = None
             local_res = evaluate(
-                series, metrics=distr_metrics, level=level, train_df=local_train
+                setup_series, metrics=distr_metrics, level=level, train_df=local_train
             )
             distr_res = fa.as_fugue_df(
                 evaluate(
-                    distributed_df, metrics=distr_metrics, level=level, train_df=distr_train
+                    distributed_df,
+                    metrics=distr_metrics,
+                    level=level,
+                    train_df=distr_train,
                 )
             ).as_pandas()
             pd.testing.assert_frame_equal(
--- tests/test_feature_engineering.py
+++ tests/test_feature_engineering.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import polars as pl
 import polars.testing
+import pytest
 
 from utilsforecast.data import generate_series
 from utilsforecast.feature_engineering import (
@@ -14,42 +15,46 @@ from utilsforecast.feature_engineering import (
     trend,
 )
 
-series = generate_series(5, equal_ends=True)
-series_pl = generate_series(5, equal_ends=True, engine='polars')
 
-
-def test_fourier_transform():
+@pytest.fixture
+def setup_series():
     series = generate_series(5, equal_ends=True)
+    series_pl = generate_series(5, equal_ends=True, engine="polars")
+    return series, series_pl
 
-    transformed_df, future_df = fourier(series, freq='D', season_length=7, k=2, h=1)
+
+def test_fourier_transform(setup_series):
+    series, series_pl = setup_series
+
+    transformed_df, future_df = fourier(series, freq="D", season_length=7, k=2, h=1)
     transformed_df2, future_df2 = fourier(
-        series.sample(frac=1.0), freq='D', season_length=7, k=2, h=1
+        series.sample(frac=1.0), freq="D", season_length=7, k=2, h=1
     )
     pd.testing.assert_frame_equal(
         transformed_df,
-        transformed_df2.sort_values(['unique_id', 'ds']).reset_index(drop=True),
+        transformed_df2.sort_values(["unique_id", "ds"]).reset_index(drop=True),
     )
     pd.testing.assert_frame_equal(future_df, future_df2)
 
-    transformed_pl, future_pl = fourier(series_pl, freq='1d', season_length=7, k=2, h=1)
+    transformed_pl, future_pl = fourier(series_pl, freq="1d", season_length=7, k=2, h=1)
     transformed_pl2, future_pl2 = fourier(
-        series_pl.sample(fraction=1.0), freq='1d', season_length=7, k=2, h=1
+        series_pl.sample(fraction=1.0), freq="1d", season_length=7, k=2, h=1
     )
     pl.testing.assert_frame_equal(transformed_pl, transformed_pl2)
     pl.testing.assert_frame_equal(future_pl, future_pl2)
     pd.testing.assert_frame_equal(
-        transformed_df.drop(columns=['unique_id']),
-        transformed_pl.drop('unique_id').to_pandas(),
+        transformed_df.drop(columns=["unique_id"]),
+        transformed_pl.drop("unique_id").to_pandas(),
     )
     pd.testing.assert_frame_equal(
-        future_df.drop(columns=['unique_id']), future_pl.drop('unique_id').to_pandas()
+        future_df.drop(columns=["unique_id"]), future_pl.drop("unique_id").to_pandas()
     )
     series = generate_series(5, equal_ends=True)
-    transformed_df, future_df = trend(series, freq='D', h=1)
+    transformed_df, future_df = trend(series, freq="D", h=1)
     transformed_df
     future_df
     transformed_df, future_df = time_features(
-        series, freq='D', features=['month', 'day', 'week'], h=1
+        series, freq="D", features=["month", "day", "week"], h=1
     )
     series_with_prices = series.assign(price=np.random.rand(len(series))).sample(
         frac=1.0
@@ -57,30 +62,30 @@ def test_fourier_transform():
     series_with_prices
     transformed_df, future_df = future_exog_to_historic(
         df=series_with_prices,
-        freq='D',
-        features=['price'],
+        freq="D",
+        features=["price"],
         h=2,
     )
     pd.testing.assert_frame_equal(
         (
-            series_with_prices.sort_values(['unique_id', 'ds'])
-            .groupby('unique_id', observed=True)
-            .tail(2)[['unique_id', 'price']]
+            series_with_prices.sort_values(["unique_id", "ds"])
+            .groupby("unique_id", observed=True)
+            .tail(2)[["unique_id", "price"]]
             .reset_index(drop=True)
         ),
-        future_df[['unique_id', 'price']],
+        future_df[["unique_id", "price"]],
     )
     series_with_prices_pl = pl.from_pandas(
-        series_with_prices.astype({'unique_id': 'int64'})
+        series_with_prices.astype({"unique_id": "int64"})
     )
     transformed_pl, future_pl = future_exog_to_historic(
         df=series_with_prices_pl,
-        freq='1d',
-        features=['price'],
+        freq="1d",
+        features=["price"],
         h=2,
     )
     pd.testing.assert_frame_equal(
-        future_pl.to_pandas(), future_df.astype({'unique_id': 'int64'})
+        future_pl.to_pandas(), future_df.astype({"unique_id": "int64"})
     )
 
 
@@ -96,63 +101,81 @@ def even_days_and_months(times):
     if isinstance(times, pd.Index):
         out = pd.DataFrame(
             {
-                'even_day': (times.weekday + 1) % 2 == 0,
-                'even_month': times.month % 2 == 0,
+                "even_day": (times.weekday + 1) % 2 == 0,
+                "even_month": times.month % 2 == 0,
             }
         )
     else:
         # for polars you can return a list of expressions
         out = [
-            (times.dt.weekday() % 2 == 0).alias('even_day'),
-            (times.dt.month() % 2 == 0).alias('even_month'),
+            (times.dt.weekday() % 2 == 0).alias("even_day"),
+            (times.dt.month() % 2 == 0).alias("even_month"),
         ]
     return out
 
 
-features = [
-    trend,
-    partial(fourier, season_length=7, k=1),
-    partial(fourier, season_length=28, k=1),
-    partial(time_features, features=['day', is_weekend, even_days_and_months]),
-]
-transformed_df, future_df = pipeline(
-    series,
-    features=features,
-    freq='D',
-    h=1,
-)
+@pytest.fixture
+def setup_features():
+    features = [
+        trend,
+        partial(fourier, season_length=7, k=1),
+        partial(fourier, season_length=28, k=1),
+        partial(time_features, features=["day", is_weekend, even_days_and_months]),
+    ]
+    return features
+
+
+@pytest.fixture
+def setup_pipeline(setup_series, setup_features):
+    def _inner(freq):
+        series, _ = setup_series
+        transformed_df, future_df = pipeline(
+            series,
+            features=setup_features,
+            freq=freq,
+            h=1,
+        )
+        return transformed_df, future_df
+
+    return _inner
 
 
 def reduce_join(dfs, on):
     return reduce(
-        lambda left, right: left.merge(right, on=on, how='left'),
+        lambda left, right: left.merge(right, on=on, how="left"),
         dfs,
     )
 
 
-def test_pipeline():
-    individual_results = [f(series, freq='D', h=1) for f in features]
+@pytest.mark.parametrize("freq", ["D", "1d"])
+def test_pipeline(setup_series, freq, setup_pipeline, setup_features):
+    series, series_pl = setup_series
+    transformed_df, future_df = setup_pipeline(freq)
+
+    individual_results = [f(series, freq=freq, h=1) for f in setup_features]
     expected_transformed = reduce_join(
-        [r[0] for r in individual_results], on=['unique_id', 'ds', 'y']
+        [r[0] for r in individual_results], on=["unique_id", "ds", "y"]
     )
     expected_future = reduce_join(
-        [r[1] for r in individual_results], on=['unique_id', 'ds']
+        [r[1] for r in individual_results], on=["unique_id", "ds"]
     )
+
     pd.testing.assert_frame_equal(transformed_df, expected_transformed)
     pd.testing.assert_frame_equal(future_df, expected_future)
+
     transformed_pl, future_pl = pipeline(
         series_pl,
-        features=features,
-        freq='1d',
+        features=setup_features,
+        freq="1d",
         h=1,
     )
     pd.testing.assert_frame_equal(
-        transformed_pl.drop('unique_id').to_pandas(),
-        transformed_df.drop(columns='unique_id'),
+        transformed_pl.drop("unique_id").to_pandas(),
+        transformed_df.drop(columns="unique_id"),
         check_dtype=False,
     )
     pd.testing.assert_frame_equal(
-        future_pl.drop('unique_id').to_pandas(),
-        future_df.drop(columns='unique_id'),
+        future_pl.drop("unique_id").to_pandas(),
+        future_df.drop(columns="unique_id"),
         check_dtype=False,
     )
