7c3c2036a1e30fbb418c8de8f01bb74b4023afe8
deven367 <masterdeven@gmail.com>
2025-06-12 09:05:35 -0400

    fix IMG_PATH and sort imports

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,5 +1,17 @@
 ### Generate synthetic data
+from functools import partial
+from pathlib import Path
+
+import numpy as np
+
 from utilsforecast.data import generate_series
+from utilsforecast.evaluation import evaluate
+from utilsforecast.losses import mape, mase
+from utilsforecast.preprocessing import fill_gaps
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+IMG_PATH = ROOT_DIR / "nbs" / "imgs"
+
 
 series = generate_series(3, with_trend=True, static_as_categorical=False)
 series
@@ -9,24 +21,16 @@ from utilsforecast.plotting import plot_series
 fig = plot_series(
     series, plot_random=False, max_insample_length=50, engine="matplotlib"
 )
-fig.savefig("imgs/index.png", bbox_inches="tight")
+fig.savefig(IMG_PATH / "index.png", bbox_inches="tight")
 
 ### Preprocessing
-from utilsforecast.preprocessing import fill_gaps
-
 serie = series[series["unique_id"].eq(0)].tail(10)
 # drop some points
 with_gaps = serie.sample(frac=0.5, random_state=0).sort_values("ds")
-with_gaps
+
 fill_gaps(with_gaps, freq="D")
+
 ### Evaluating
-from functools import partial
-
-import numpy as np
-
-from utilsforecast.evaluation import evaluate
-from utilsforecast.losses import mape, mase
-
 valid = series.groupby("unique_id").tail(7).copy()
 train = series.drop(valid.index)
 rng = np.random.RandomState(0)
